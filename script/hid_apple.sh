b53147efd9af57801af51b10f2712bcb16fce9fb
zengshuai <zengs1994@gmail.com>
2021-10-28 19:35:30 +0800

    add keychron keyboard script; add wordmotion plugin to vim

--- script/hid_apple.sh
+++ script/hid_apple.sh
@@ -6,6 +6,8 @@
 # replace <value> below with the one that worked for you in the previous step (0, 1 or 2)
 # example: echo "options hid_apple fnmode=2 | sudo tee /etc/modprobe.d/hid_apple.conf"
 # this will erase any pre-existing contents from /etc/modprobe.d/hid_apple.conf
-echo "options hid_apple fnmode=<value>" | sudo tee /etc/modprobe.d/hid_apple.conf
-sudo update-initramfs -u
-sudo systemctl reboot
+echo "options hid_apple fnmode=2" | sudo tee /etc/modprobe.d/hid_apple.conf
+# change below file to 2
+sudo nvim /sys/module/hid_apple/parameters/fnmode
+# refresh to img
+sudo dracut -f /boot/initramfs-5.14.13-200.fc34.x86_64.img
