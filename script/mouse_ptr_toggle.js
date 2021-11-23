1e69c749117cc532d2f5e9ac4687c2a7b30da513
zengshuai <zengs1994@gmail.com>
2021-11-23 23:37:54 +0800

    press ant key to hide and move mouse to show

--- script/mouse_ptr_toggle.js
+++ script/mouse_ptr_toggle.js
@@ -5,12 +5,10 @@ function toggleCursor() {
     body_dom.style.cursor = 'default';
   }
 }
-function keyToggleCursor(event) {
-  if (event.keyCode == 27) {
-    toggleCursor();
-  }
-}
 var body_dom = document.getElementsByTagName('body')[0];
 if (body_dom) {
-  body_dom.onkeydown = keyToggleCursor;
+  body_dom.onkeydown = toggleCursor;
+  body_dom.onmousemove = function () {
+    body_dom.style.cursor = 'default';
+  };
 }
