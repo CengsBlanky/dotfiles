e864640938fdbe6f93c37fd7e35852a2dae6b04c
zengshuai <zengs1994@gmail.com>
2022-02-21 16:27:08 +0800

    force fish change vi cursor in tmux

--- fish/config.fish
+++ fish/config.fish
@@ -6,14 +6,15 @@ if status is-interactive
     fish_vi_key_bindings
     # Emulates vim's cursor shape behavior
     # Set the normal and visual mode cursors to a block
-    set fish_cursor_default block
+    set -U fish_cursor_default block
     # Set the insert mode cursor to a line
-    set fish_cursor_insert line
+    set -U fish_cursor_insert line
     # Set the replace mode cursor to an underscore
-    set fish_cursor_replace_one underscore
+    set -U fish_cursor_replace_one underscore
     # The following variable can be used to configure cursor shape in
     # visual mode, but due to fish_cursor_default, is redundant here
-    set fish_cursor_visual block
+    set -U fish_cursor_visual block
+    set -g fish_vi_force_cursor 1
 
     # ===== gloabal variables =====
     set -Ux LANG "en_US.UTF-8"
