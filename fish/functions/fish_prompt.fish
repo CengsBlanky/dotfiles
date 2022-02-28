98ce957a8f0a54c5ec9b7b02812d01d55e5c6401
zengshuai <zengs1994@gmail.com>
2022-03-01 00:42:52 +0800

    update fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -7,7 +7,6 @@ function fish_prompt --description 'Write out the prompt'
         (set_color 868e96) \
         (set_color $user_host_color) (hostname | cut -d . -f 1) \
         (set_color 868e96) \
-        (set_color normal)
 
     # PWD
     set_color $user_cwd_color
@@ -22,7 +21,7 @@ function fish_prompt --description 'Write out the prompt'
 
     __terlar_git_prompt
     fish_hg_prompt
-    echo
+    printf '\n'
 
     set_color $user_prompt_symbol_color
 
@@ -30,19 +29,6 @@ function fish_prompt --description 'Write out the prompt'
         set_color $fish_color_error
     end
 
-    switch $fish_bind_mode
-    case default
-        set -l prompt_color 5e81ac
-    case insert
-        set -l prompt_color a3be8c
-    case replace_one
-        set -l prompt_color fdb924
-    case visual
-        set -l prompt_color a5d8ff
-    case '*'
-        set -l prompt_color red
-    end
-    set_color -b $prompt_color
     echo -n '➤ '
     set_color normal
 end
