5309151cd107e2c98005757ef1ab7b080dd94b8e
zengshuai <zengs1994@gmail.com>
2022-03-10 11:18:47 +0800

    update fish git prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -11,9 +11,7 @@ function fish_prompt --description 'Write out the prompt'
     set -g __fish_git_prompt_showcolorhints true
     set -g __fish_git_prompt_showdirtystate true
     set -g __fish_git_prompt_showuntrackedfiles true
-    # set -g __fish_git_prompt_show_informative_status true
     set -g __fish_git_prompt_use_informative_chars true
-
 
     printf '%s%s%s@%s%s%s:%s ' \
         (set_color $user_name_color) $USER \
