e19bfb07f1d47cf37e326d6934e7b439e613237e
zengshuai <zengs1994@gmail.com>
2022-03-10 17:02:34 +0800

    update fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,11 +1,24 @@
 function fish_prompt --description 'Write out the prompt'
-    set -l last_status $status
+    set -l user_name_color '4c6a92'
+    set -l user_host_color '4c6a92'
+    set -l user_cwd_color '8fbcbb'
+    set -l user_prompt_symbol_color '2b8a3e'
     set -l red_color 'ff2b2b'
     set -l gray_color '868e96'
 
-    printf '%s[%s%s%s@%s%s%s]%s ' \
-        (set_color $gray_color) \
-        (set_color $user_host_color) $USER \
+    set -l last_status $status
+
+    set -g __fish_git_prompt_showcolorhints true
+    set -g __fish_git_prompt_showdirtystate true
+    set -g __fish_git_prompt_showuntrackedfiles true
+    set -g __fish_git_prompt_showupstream auto
+    set -g __fish_git_prompt_char_upstream_ahead ⇡
+    set -g __fish_git_prompt_char_upstream_behind ⇣
+    set -g __fish_git_prompt_char_upstream_diverged ⇵
+    set -g __fish_git_prompt_char_upstream_equal ""
+
+    printf '%s%s%s@%s%s%s:%s ' \
+        (set_color $user_name_color) $USER \
         (set_color $gray_color) \
         (set_color $user_host_color) (prompt_hostname) \
         (set_color $gray_color)
@@ -20,8 +33,8 @@ function fish_prompt --description 'Write out the prompt'
 
     set_color normal
 
-    __terlar_git_prompt
-    fish_hg_prompt
+    # __terlar_git_prompt
+    fish_git_prompt
 
     echo;
 
@@ -31,6 +44,7 @@ function fish_prompt --description 'Write out the prompt'
         set_color $red_color
     end
 
-    echo -n '➤ '
+    # echo -n '➤ '
+    echo -n '$ '
     set_color normal
 end
