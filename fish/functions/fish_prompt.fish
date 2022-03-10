061838e0ce8b7e4dd4c17c80f244aa991ff956f9
zengshuai <zengs1994@gmail.com>
2022-03-10 11:29:59 +0800

    add char icon

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -13,6 +13,9 @@ function fish_prompt --description 'Write out the prompt'
     set -g __fish_git_prompt_showuntrackedfiles true
     set -g __fish_git_prompt_use_informative_chars true
     set -g __fish_git_prompt_showupstream auto
+    set -g __fish_git_prompt_char_upstream_ahead ⇡
+    set -g __fish_git_prompt_char_upstream_behind ⇣
+    set -g __fish_git_prompt_char_upstream_diverged ⇵
     set -g __fish_git_prompt_char_upstream_equal ""
 
     printf '%s%s%s@%s%s%s:%s ' \
