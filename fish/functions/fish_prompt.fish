bcd12999c579d0edf1be49a950608d524410ca78
zengshuai <zengs1994@gmail.com>
2022-03-16 22:03:55 +0800

    update fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -12,9 +12,9 @@ function fish_prompt --description 'Write out the prompt'
     set -g __fish_git_prompt_showdirtystate true
     set -g __fish_git_prompt_showuntrackedfiles true
     set -g __fish_git_prompt_showupstream auto
-    set -g __fish_git_prompt_char_upstream_ahead ⇡
-    set -g __fish_git_prompt_char_upstream_behind ⇣
-    set -g __fish_git_prompt_char_upstream_diverged ⇵
+    set -g __fish_git_prompt_char_upstream_ahead ' ⇡'
+    set -g __fish_git_prompt_char_upstream_behind ' ⇣'
+    set -g __fish_git_prompt_char_upstream_diverged ' ⇵'
     set -g __fish_git_prompt_char_upstream_equal ""
 
     printf '%s%s%s@%s%s%s:%s ' \
