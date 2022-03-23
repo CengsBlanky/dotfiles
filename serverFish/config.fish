9d87ff43b51742ce32d3f2e58db95ee2f983bc25
zengshuai <zengs1994@gmail.com>
2022-03-23 10:41:48 +0000

    add server side fish shell config

---
+++ serverFish/config.fish
@@ -0,0 +1,13 @@
+set -g fish_prompt_pwd_dir_length 0
+set -gx EDITOR vim
+fish_vi_key_bindings
+abbr --add vims "sudo vim"
+abbr --add l "ls -lh"
+abbr --add ll "ls -lAh"
+abbr --add cls "clear"
+abbr --add gst "git status"
+abbr --add gad "git add"
+abbr --add gcm "git commit -am"
+abbr --add gpl "git pull"
+abbr --add gps "git push"
+abbr --add clashstate "systemctl status clash"
