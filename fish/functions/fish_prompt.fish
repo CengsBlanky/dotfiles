96774c0a1ac4a9ff8b162ff0fd965ea634f56d59
zengshuai <zengs1994@gmail.com>
2022-03-30 21:40:43 +0800

    chane fish shell color

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,7 +1,7 @@
 function fish_prompt --description '[user]@[host]: pwd [prompt_symbol]'
     set -l user_name_color '4c6a92'
-    set -l user_host_color '4c6a92'
-    set -l user_cwd_color '8fbcbb'
+    set -l user_host_color $user_name_color
+    set -l pwd_color 'a3be8c'
     set -l user_prompt_symbol_color '2b8a3e'
     set -l red_color 'ff2b2b'
     set -l gray_color '868e96'
@@ -13,7 +13,7 @@ function fish_prompt --description '[user]@[host]: pwd [prompt_symbol]'
     #     (set_color $user_host_color) (prompt_hostname) \
     #     (set_color $gray_color)
     # PWD
-    set_color $user_cwd_color
+    set_color $pwd_color
     echo -n (prompt_pwd)
 
     if not test $last_status -eq 0
