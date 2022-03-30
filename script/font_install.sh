53441f44d929aa68082427fd9ee32204fe845c28
zengshuai <zengs1994@gmail.com>
2022-03-31 00:23:28 +0800

    update scripts; clear README file

--- script/font_install.sh
+++
@@ -1,9 +0,0 @@
-#! bash
-# @since   2021-10-29 19:09
-# @author  zengshuai
-# @version 1.0
-
-sudo mkdir -p /usr/share/fonts/<FontName>
-sudo mv <font file> /usr/share/fonts/
-# refresh cache -f means force refresh, -v means verbose
-sudo fc-cache -f -v
