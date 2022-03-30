b218b7dd718d47fa75231ab9ae4c3a7d5c74be5b
zengshuai <zengs1994@gmail.com>
2022-03-30 21:18:35 +0800

    change fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,4 +1,4 @@
-function fish_prompt --description 'Write out the prompt'
+function fish_prompt --description '[user]@[host]: pwd [prompt_symbol]'
     set -l user_name_color '4c6a92'
     set -l user_host_color '4c6a92'
     set -l user_cwd_color '8fbcbb'
@@ -6,44 +6,26 @@ function fish_prompt --description 'Write out the prompt'
     set -l red_color 'ff2b2b'
     set -l gray_color '868e96'
 
-    set -l last_status $status
-
-    set -g __fish_git_prompt_showcolorhints true
-    set -g __fish_git_prompt_showdirtystate true
-    set -g __fish_git_prompt_showuntrackedfiles true
-    set -g __fish_git_prompt_showupstream auto
-    set -g __fish_git_prompt_char_upstream_ahead ' ⇡'
-    set -g __fish_git_prompt_char_upstream_behind ' ⇣'
-    set -g __fish_git_prompt_char_upstream_diverged ' ⇵'
-    set -g __fish_git_prompt_char_upstream_equal ""
-
-    printf '%s%s%s@%s%s%s:%s ' \
-        (set_color $user_name_color) $USER \
-        (set_color $gray_color) \
-        (set_color $user_host_color) (prompt_hostname) \
-        (set_color $gray_color)
+    # printf '%s%s%s@%s%s%s%s' \
+    #     (set_color $user_name_color) $USER \
+    #     (set_color $gray_color) \
+    #     (set_color $user_host_color) (prompt_hostname) \
+    #     (set_color $gray_color)
     # PWD
     set_color $user_cwd_color
     echo -n (prompt_pwd)
 
-    if not test $last_status -eq 0
+    if not test $status -eq 0
         set_color $red_color
-        printf ' [%s]' $last_status
+        printf ' [%s]' $status
     end
-
-    set_color normal
-
-    # __terlar_git_prompt
-    fish_git_prompt
-
-    echo;
 
     set_color $user_prompt_symbol_color
 
-    if not test $last_status -eq 0
+    if not test $status -eq 0
         set_color $red_color
     end
 
-    echo -n ' '
+    echo -n '  '
     set_color normal
 end
