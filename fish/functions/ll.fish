ac251d70ce6d35f6f7960dc7296e56d29ffbab60
zengshuai <zengs1994@gmail.com>
2022-05-24 20:46:12 +0800

    update

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,6 +1,6 @@
 function ll
     if command -q exa
-        exa -l --color always --icons -a -s type $argv
+        exa -l --color always --icons -a -s type --git $argv
     else
         ls -lAh $argv
     end
