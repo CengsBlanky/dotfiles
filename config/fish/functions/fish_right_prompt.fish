b1718e350ad2a3cc70e897fa0aa06dd2c0097e97
zengshuai <zengs1994@gmail.com>
2022-06-14 19:38:17 +0800

    update fish right prompt

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -1,4 +1,14 @@
 function fish_right_prompt
     # set_color -o
+    set -g __fish_git_prompt_showcolorhints true
+    set -g __fish_git_prompt_showdirtystate true
+    set -g __fish_git_prompt_showuntrackedfiles true
+    # set -g __fish_git_prompt_showupstream auto
+    set -g __fish_git_prompt_char_stateseparator ' '
+    set -g __fish_git_prompt_char_upstream_ahead ''
+    set -g __fish_git_prompt_char_upstream_behind ''
+    set -g __fish_git_prompt_char_upstream_diverged ''
+    set -g __fish_git_prompt_char_upstream_equal ""
+
     echo -n (fish_git_prompt)
 end
