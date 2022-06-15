82824286e32f371ce89754a04d96c7a3c5d922f2
zengshuai <zengs1994@gmail.com>
2022-06-15 23:25:10 +0800

    update fish icons

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -1,6 +1,7 @@
 function fish_right_prompt
     set -g __fish_git_prompt_show_informative_status 0
     set -g __fish_git_prompt_hide_untrackedfiles 1
+    set -g __fish_git_prompt_char_stateseparator " "
 
     set -g __fish_git_prompt_color_branch magenta
     set -g __fish_git_prompt_showupstream "informative"
