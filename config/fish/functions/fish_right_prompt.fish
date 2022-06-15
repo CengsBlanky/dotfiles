30bd9d3643c4359b315ef8a53f063982019f8d8e
zengshuai <zengs1994@gmail.com>
2022-06-15 23:23:00 +0800

    update fish icons

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -1,5 +1,5 @@
 function fish_right_prompt
-    set -g __fish_git_prompt_show_informative_status 1
+    set -g __fish_git_prompt_show_informative_status 0
     set -g __fish_git_prompt_hide_untrackedfiles 1
 
     set -g __fish_git_prompt_color_branch magenta
@@ -9,10 +9,10 @@ function fish_right_prompt
     set -g __fish_git_prompt_char_upstream_prefix ""
 
     set -g __fish_git_prompt_char_stagedstate "●"
-    set -g __fish_git_prompt_char_dirtystate "+"
-    set -g __fish_git_prompt_char_untrackedfiles ""
-    set -g __fish_git_prompt_char_conflictedstate ""
-    set -g __fish_git_prompt_char_cleanstate ""
+    set -g __fish_git_prompt_char_dirtystate "*"
+    set -g __fish_git_prompt_char_untrackedfiles "+"
+    set -g __fish_git_prompt_char_conflictedstate ""
+    set -g __fish_git_prompt_char_cleanstate ""
 
     set -g __fish_git_prompt_color_dirtystate blue
     set -g __fish_git_prompt_color_stagedstate yellow
