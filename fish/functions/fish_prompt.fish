ad7d1df73cb23ef87fa4a4f8d101f56676c8e25d
zengshuai <zengs1994@gmail.com>
2022-06-20 21:50:43 +0800

    update fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,8 @@
 function fish_prompt
     set -l prompt_color '66a80f'
+    set -l pwd_color '5e81ac'
+    
+    set_color $pwd_color
     echo -n (basename (prompt_pwd))
     set_color $prompt_color
     echo -n '  '
