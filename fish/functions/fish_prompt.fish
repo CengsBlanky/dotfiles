8b55bbf0513bc9318f998afae3caf815079cc417
zengshuai <zengs1994@gmail.com>
2022-06-20 21:49:21 +0800

    update fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,29 +1,7 @@
 function fish_prompt
-    set -l pwd_color '8fbcbb'
-    set -l err_color 'c92a2a'
-    set -l gray_color '868e96'
-    set -l dir_background '434c5e'
-    set -l cherry_color 'fa5252'
-    set -l banana_color 'ffd43b'
-    set -l square_color '74c0fc'
-
-    set -l last_status $status
-    set_color -o $pwd_color
-    set_color -b $dir_background
-
-    set_color -o $square_color
-    echo -n 'ﯟ '
-    set_color -o $pwd_color
-
-    echo -n (prompt_pwd)
-    
-    if not test $last_status -eq 0
-        set_color -o $err_color
-        printf ' %s' $last_status
-    end
-
-    set_color -b normal
-    set_color $dir_background
-    echo -n ''
+    set -l prompt_color '66a80f'
+    echo -n (basename (prompt_pwd))
+    set_color $prompt_color
+    echo -n '  '
     set_color normal
 end
