0f2de25d7dcaf1c450ee060bb322ac378f6925a0
zengshuai <zengs1994@gmail.com>
2022-09-18 18:07:58 +0800

    add install software

--- DebianSetup/install.sh
+++ DebianSetup/install.sh
@@ -1,2 +1,2 @@
-sudo apt install flatpak vim tmux ranger bat kdeconnect snapd netselect-apt pandoc ttf-wqy-zenhei
+sudo apt install flatpak vim tmux ranger bat kdeconnect snapd netselect-apt pandoc ttf-wqy-zenhei ffmpeg
 sudo snap install neovim --classic
