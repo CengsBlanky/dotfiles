d0a8b9f958c9b60856584ef507e890eb9219bf2b
zengshuai <zengs1994@gmail.com>
2022-10-30 12:12:37 +0800

    add my nerdfont patcher script

---
+++ script/my_patcher.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+./font-patcher --windows --complete --out $HOME/.fonts/ --progressbars \
+    --fontawesome \
+    --fontawesomeextension \
+    --fontlogos \
+    --fontlinux \
+    --octicons \
+    --codicons \
+    --powersymbols \
+    --pomicons \
+    --powerline \
+    --powerlineextra \
+    --material \
+    --weather \
+    $1
