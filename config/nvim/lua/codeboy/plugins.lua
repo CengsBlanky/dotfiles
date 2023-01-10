593b2c0d049847f4d24c859bc67ddcb808a9ed68
zengshuai <zengs1994@gmail.com>
2023-01-10 22:54:00 +0800

    nvim add open buffers map

--- config/nvim/lua/codeboy/plugins.lua
+++ config/nvim/lua/codeboy/plugins.lua
@@ -105,6 +105,7 @@ vim.keymap.set('n', '<M-\\>', ':TmuxNavigatePrevious<CR>', { silent = true })
 --- telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 require('telescope').load_extension('fzf')
 -- neoformat
