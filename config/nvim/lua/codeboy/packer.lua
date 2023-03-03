aa75cdac82fc5e8e8f41f4999a90f0e23300dcbd
zengshuai <zengs1994@gmail.com>
2023-03-04 00:41:12 +0800

    nvim add lualine plugin

--- config/nvim/lua/codeboy/packer.lua
+++ config/nvim/lua/codeboy/packer.lua
@@ -46,6 +46,10 @@ local packerStartup = require('packer').startup({function(use)
   use 'williamboman/mason-lspconfig.nvim'
   use 'sbdchd/neoformat'
   use {"EdenEast/nightfox.nvim", run = ":NightfoxCompile",}
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
@@ -140,6 +144,49 @@ require'nvim-treesitter.configs'.setup {
     enable = true, -- mandatory, false will disable the whole extension
     disable = {},  -- optional, list of language that will be disabled
   }
+}
+--- lualine
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {
+      {
+        'filename',
+        symbols = {
+          modified = '●',      -- Text to show when the buffer is modified
+          alternate_file = '#', -- Text to show to identify the alternate file
+          directory =  '',     -- Text to show when the buffer is a directory
+        },
+
+      }
+    },
+    lualine_c = {'diagnostics'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'%P'},
+    lualine_z = {'%l/%L'}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
 }
 
 -- last line
