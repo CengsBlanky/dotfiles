adffebe6d4f4903b54ce7544a9cc840fdd3f8610
zengshuai <zengs1994@gmail.com>
2023-06-07 00:27:18 +0800

    update nerdfont patcher script

--- script/nerdfont_patcher.sh
+++ script/nerdfont_patcher.sh
@@ -4,21 +4,8 @@ source_fonts_dir="$1"
 target_fonts_dir="$2"
 
 for font_name in $source_fonts_dir/*.ttf; do
-    ./font-patcher \
-        --windows \
+    fontforge --script ./font-patcher \
         --complete \
-        --progressbars \
-        --fontawesome \
-        --fontawesomeextension \
-        --fontlogos \
-        --fontlinux \
-        --octicons \
-        --codicons \
-        --powersymbols \
-        --pomicons \
-        --powerline \
-        --powerlineextra \
-        --weather \
         $font_name \
         --out $target_fonts_dir
 done
