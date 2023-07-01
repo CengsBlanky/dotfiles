919f2595e427eb10ca1132a5fc90f210d4700f39
zengshuai <zengs1994@gmail.com>
2023-07-01 21:39:00 +0800

    update fish prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,31 +1,16 @@
 function fish_prompt
     set -l last_status $status
-    set -l icon_color '000000'
-    set -l icon_bg_color '868e96'
-    set -l pwd_color '2e3440'
-    set -l pwd_bg_color 'adb5bd'
+    set -l pwd_color_bare '88c0d0'
     set -l err_color 'c92a2a'
-    set -l err_bg_color 'd8dee9'
+    set -l prompt_icon_color 'a3be8c'
 
-    set_color $icon_color
-    set_color -b $icon_bg_color
-    echo -n " "
-    set_color -b $pwd_bg_color
-    set_color $icon_bg_color
-    echo -n ""
-
-    set_color $pwd_color
+    set_color -o $pwd_color_bare
     echo -n (basename (prompt_pwd))
-    set_color $pwd_bg_color
-    set_color -ob $err_bg_color
-    echo -n ""
-
     if not test $last_status -eq 0
         set_color -o $err_color
-        printf '%s' $last_status
+        printf ' %s' $last_status
     end
-    set_color -b normal
-    set_color -o $err_bg_color
-    echo -n ""
+    set_color -o $prompt_icon_color
+    echo -n "  "
     set_color normal
 end
