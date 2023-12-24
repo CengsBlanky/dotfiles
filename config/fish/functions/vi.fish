6f10382b61033f5f303b2d1e9323a2930637cd6c
zengshuai <zengs1994@gmail.com>
2023-12-24 10:31:18 +0800

    update fish function

--- config/fish/functions/vi.fish
+++ config/fish/functions/vi.fish
@@ -1,7 +1,9 @@
 function vi
-    if command -q /usr/local/bin/vim
+    if command -q /usr/bin/vi
+        /usr/bin/vi $argv
+    else if command -q /usr/local/bin/vim
         /usr/local/bin/vim $argv
-    else
-        vim $argv
+    else if command -q /usr/bin/vim
+        /usr/bin/vim $argv
     end
 end
