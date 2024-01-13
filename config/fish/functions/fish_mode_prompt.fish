4d7a8480da73052e04cb026b3e8eb54f672303da
zengshuai <zengs1994@gmail.com>
2024-01-13 22:11:28 +0800

    update fish mode icon

--- config/fish/functions/fish_mode_prompt.fish
+++ config/fish/functions/fish_mode_prompt.fish
@@ -1,17 +1,17 @@
 function fish_mode_prompt
     switch $fish_bind_mode
         case default
-            set_color --bold blue
+            set_color --bold "5f3dc4"
             echo " "
         case insert
             set_color --bold green
             echo " "
         case visual
             set_color --bold yellow
-            echo " "
+            echo " "
         case replace_one
             set_color --bold brmagenta
-            echo " "
+            echo " "
         case '*'
             set_color --bold red
             printf "󰊠 "
