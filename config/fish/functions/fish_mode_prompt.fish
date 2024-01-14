1c266a0178b94b7394e22b07b476893f39811a65
zengshuai <zengs1994@gmail.com>
2024-01-14 11:41:08 +0800

    update fish prompt

--- config/fish/functions/fish_mode_prompt.fish
+++ config/fish/functions/fish_mode_prompt.fish
@@ -2,23 +2,18 @@ function fish_mode_prompt
     switch $fish_bind_mode
         case default
             set_color --bold "81a1c1"
-            echo " "
         case insert
             set_color --bold "a3be8c"
-            echo " "
         case visual
             set_color --bold yellow
-            echo " "
         case replace_one
             set_color --bold brmagenta
-            echo " "
         case '*'
             set_color --bold red
-            printf "󰊠 "
     end
     # set_color --bold "81a1c1"
     echo "$USER"
-    set_color --bold "d08770"
+    set_color --bold "4c566a"
     echo "@"
     set_color normal
 end
