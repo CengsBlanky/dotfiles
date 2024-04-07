8026ad331bcf0a94ee6ace0e0a3f5810a0d0f972
zengshuai <zengs1994@gmail.com>
2024-04-07 22:13:02 +0800

    update cat fish function

--- config/fish/functions/cat.fish
+++ config/fish/functions/cat.fish
@@ -1,5 +1,7 @@
 function cat
-    if command -q bat
+    if test (count $argv) -eq 1; and string match "*.md" $argv[1]; and command -q glow
+        glow $argv
+    else if command -q bat
         bat -pp --theme="Nord" $argv
     else if command -q batcat
         batcat --style=plain --theme="base16" $argv
