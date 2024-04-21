aa0db77edb21443e8250bf39c8937b8ad31c3130
zengshuai <zengs1994@gmail.com>
2024-04-21 18:29:50 +0800

    use <leader>r to rename in lsp

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -272,7 +272,7 @@ require("lazy").setup({
         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
         -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
         vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-        vim.keymap.set('n', '<leader>n', vim.lsp.buf.rename, bufopts)
+        vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, bufopts)
         vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, bufopts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
         vim.keymap.set('n', '<F4>', function() vim.lsp.buf.format { async = true } end, bufopts)
