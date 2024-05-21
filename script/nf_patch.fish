534357d1187778045761f54f64bfff433989683b
zengshuai <zengs1994@gmail.com>
2024-05-21 19:51:33 +0800

    add Iosevka custom font config; update font patcher

--- script/nf_patch.fish
+++ script/nf_patch.fish
@@ -13,10 +13,20 @@ mkdir -p $target_dir
 set -l font_files (find $src_dir -maxdepth 1 -name "*.ttf" -type f )
 set -l font_count (count $font_files)
 echo found $font_count font file\(s\) to patch...
+echo "patched font file will save to $target_dir"
+set -l patched_count 0
 
 for font_file in $font_files
-    fontforge --script $HOME/Apps/FontPatcher/font-patcher --no-progressbars --adjust-line-height --careful --complete $font_file --out $target_dir &> patch.log
-    printf "%s %s\n" $font_file "patched finished, see patch.log for details"
+    set -l err_log "$font_file.err"
+    fontforge --script $HOME/Apps/FontPatcher/font-patcher --no-progressbars --adjust-line-height --careful --complete $font_file --out $target_dir &> $err_log
+    # check patch result
+    if not test $status -eq 0
+        echo "$font_file patch failed! check error log: $err_log for details"
+    else
+        rm $err_log
+        echo "$font_file patched finished."
+        set patched_count (math $patched_count + 1)
+    end
 end
 
-echo "font patch done!"
+echo "$patched_count out of $font_count font patched done!"
