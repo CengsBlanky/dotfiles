7efa8364652dee66bd8393ae699aa0bf501f094b
zengshuai <zengs1994@gmail.com>
2024-05-23 21:59:27 +0800

    update vim and nvim statusline with no plugin

--- config/nvim/lua/config/statusline.lua
+++ config/nvim/lua/config/statusline.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-global
 function _G.lspStatusline()
   local count = {}
   local levels = {
@@ -17,20 +18,23 @@ function _G.lspStatusline()
   local info = ""
 
   if count["errors"] ~= 0 then
-    errors = " %#LspError#󰅙 " .. count["errors"]
+    errors = " %#DiagnosticVirtualTextError#󰅙 " .. count["errors"]
   end
   if count["warnings"] ~= 0 then
-    warnings = " %#LspWarn# " .. count["warnings"]
+    warnings = " %#DiagnosticVirtualTextWarn# " .. count["warnings"]
   end
   if count["hints"] ~= 0 then
-    hints = " %#LspHint#󰌵 " .. count["hints"]
+    hints = " %#DiagnosticVirtualTextHint#󰌵 " .. count["hints"]
   end
   if count["info"] ~= 0 then
-    info = " %#LspInfo#󰋼 " .. count["info"]
+    info = " %#DiagnosticVirtualTextInfo#󰋼 " .. count["info"]
   end
 
   return errors .. warnings .. hints .. info .. "%0*"
 end
 
 -- statusline
-vim.opt.statusline = '%-5r%f%m%{%v:lua.lspStatusline()%}%=%-(%{&fileformat}%Y%)%12P%16(%l,%c/%L%)'
+vim.opt.statusline = '%-5r%f%m%{%v:lua.lspStatusline()%}%=%-10(%{&fileformat}%Y%)%12P%16(%l,%c/%L%)'
+
+-- todo
+-- show lsp progress
