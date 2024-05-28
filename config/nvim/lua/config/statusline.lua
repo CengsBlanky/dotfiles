769e10b2352ac1a0411683abf5dee7e7a3ab0c8f
zengshuai <zengs1994@gmail.com>
2024-05-29 00:54:23 +0800

    update statusline and bufferline

--- config/nvim/lua/config/statusline.lua
+++ config/nvim/lua/config/statusline.lua
@@ -34,7 +34,7 @@ function _G.lspStatusline()
 end
 
 -- statusline
-vim.opt.statusline = '%-5r%f%4m%{%v:lua.lspStatusline()%}%=%-10(%{&fileformat}%Y%)%12P%16(%l,%c/%L%)'
+vim.opt.statusline = '%-5r%f%4m%{%v:lua.lspStatusline()%}%=%15(%{&fileformat}%Y%)%12P%16(%l,%c/%L%)'
 
 -- todo
 -- show lsp progress
