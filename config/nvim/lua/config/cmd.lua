1d408ae60260b29cfa5757ff2faf4017eb70b435
CengsBlanky <52321832+CengsBlanky@users.noreply.github.com>
2024-06-21 17:52:26 +0800

    Update cmd.lua add command

--- config/nvim/lua/config/cmd.lua
+++ config/nvim/lua/config/cmd.lua
@@ -1,3 +1,13 @@
 ---@diagnostic disable: undefined-global
 local init_file = "~/.config/nvim/init.lua"
 vim.api.nvim_create_user_command('Reloadrc', 'source ' .. init_file, {})
+-- copy current filename to clipboard
+vim.api.nvim_create_user_command('Pname',
+  function(opts)
+    vim.fn.setreg('+', vim.fn.expand('%:p'))
+  end, {})
+
+vim.api.nvim_create_user_command('Fname',
+  function(opts)
+    vim.fn.setreg('+', vim.fn.expand('%:t'))
+  end, {})
