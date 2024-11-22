b9da64e55b3894a711b968dc6483788893af810c
zengshuai <zengs1994@gmail.com>
2024-11-22 23:24:37 +0800

    change font; update n/vim statusline to show encoding

--- config/nvim/lua/zeng/statusline.lua
+++ config/nvim/lua/zeng/statusline.lua
@@ -34,7 +34,7 @@ function _G.lspStatusline()
 end
 
 -- statusline
-vim.opt.statusline = '[%{&fileencoding}]%r %f%m%{%v:lua.lspStatusline()%}%=%(%{&fileformat}%Y%)%12(%l/%L%)'
+vim.opt.statusline = '%r %f%m%{%v:lua.lspStatusline()%}%=%l/%L%-12(  %{&fileencoding}%)%{&fileformat}%Y'
 
 -- todo
 -- show lsp progress
