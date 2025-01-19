ce24f6ad7fb34f570363214612c23dd5d5103ef3
zengshuai <zengs1994@gmail.com>
2025-01-19 20:00:12 +0800

    update tmux and nvim config

--- config/nvim/lua/zeng/base.lua
+++ config/nvim/lua/zeng/base.lua
@@ -5,6 +5,7 @@ vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 5
 vim.opt.title = true
 vim.opt.wildmenu = true
 vim.opt.showmatch = true
