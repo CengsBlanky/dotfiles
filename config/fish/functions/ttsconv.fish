952a1d26997307cd536c33157585cce6b856e7c8
zengshuai <zengs1994@gmail.com>
2025-04-10 00:41:07 +0800

    update ttsonv function to automation

--- config/fish/functions/ttsconv.fish
+++ config/fish/functions/ttsconv.fish
@@ -1,11 +1,51 @@
 function ttsconv
-    set input_txt $argv[1]
-    set voice $argv[2]
+    set -l ebook_name (realpath $argv[1])
+    set -l voice $argv[2]
 
-    if test -z "$voice"
-        set voice "zh-CN-YunyangNeural"
+    echo "start convert ebook: $ebook_name"
+    set -l book_basename (path basename $ebook_name)
+    set -l origin_extension (path extension $ebook_name)
+    set -l text_book (path change-extension "txt" $book_basename)
+    set -l audio_book (path change-extension "mp3" $book_basename)
+
+    set -l suffix_name (path change-extension "" $book_basename)
+    set -l tmp_dir $HOME/tmp/ttsconv_$suffix_name
+    mkdir -p $tmp_dir
+    echo "create temp directory: $tmp_dir"
+    cd $tmp_dir
+
+    if test $origin_extension = ".txt"
+        cp $ebook_name ./
+    else
+        echo (string join " " "convert" $ebook_name "to" $text_book "...")
+        ebook-convert $ebook_name $text_book
     end
-    set mp3_file (path change-extension mp3 $input_txt)
-    echo "convert $input_txt to $mp3_file start..."
-    edge-tts -f $input_txt -v $voice --write-media $mp3_file
+    sed -i '/^ *$/d' $text_book
+    echo (string join " " "total lines:" (wc -l $text_book))
+
+    set -l concatfile "files.txt"
+    if test -e $concatfile
+        rm $concatfile
+    end
+    split -l 50 -d $text_book --additional-suffix=split_text
+    set -l split_files (ls -1 | grep -E "^x[0-9]+split_text\$")
+    set -l split_file_count (count $split_files)
+    set -l nth_count 1
+    for file in $split_files
+        set -l mp3_file (string join "" "tmp_" (path change-extension mp3 $file))
+        if test -z "$voice"
+            set voice "zh-CN-YunyangNeural"
+        end
+        echo "convert $file to $mp3_file start -> "(math "100 * $nth_count / $split_file_count")"%"
+        edge-tts -f $file -v $voice --write-media $mp3_file
+        while test $status -ne 0; edge-tts -f $file -v $voice --write-media $mp3_file; end
+        echo "file '$mp3_file'" >> $concatfile
+        set nth_count (math $nth_count + 1)
+    end
+    ffmpeg -f concat -safe 0 -i $concatfile -c:a copy $audio_book
+    if test $status -eq 0
+        echo "remove temp files..."
+        rm tmp_*.mp3 $split_files $text_book $concatfile
+        echo "audio file save to "(realpath $audio_book)
+    end
 end
