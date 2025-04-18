49ed916f4bc9ae81988792849dda57048272a2b6
zengshuai <zengs1994@gmail.com>
2025-04-19 00:15:24 +0800

    add abbr

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -113,6 +113,8 @@ if status is-interactive
     abbr --add hibernate "sudo pm-hibernate"
     # take little notes
     abbr --add nt notenow
+    # goto dotfiles
+    abbr --add cddot cd ~/.dotfiles
     # yt-dlp
     abbr --add yd --set-cursor "yt-dlp '%'"
     # find files
