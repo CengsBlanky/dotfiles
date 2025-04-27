3ea26ba17e88a7a521931671852dfcfe970a507d
zengshuai <zengs1994@gmail.com>
2025-04-27 21:08:40 +0800

    update n/vim command

--- config/nvim/plugin/cmd.lua
+++ config/nvim/plugin/cmd.lua
@@ -3,12 +3,29 @@ local init_file = "~/.config/nvim/init.lua"
 local command = vim.api.nvim_create_user_command
 command('Reloadrc', 'source ' .. init_file, {})
 -- copy current filename to clipboard
-command('Pname',
+command('Rname',
   function()
     vim.fn.setreg('+', vim.fn.expand('%'))
-  end, {})
+  end,
+  {
+    desc = "relative filename",
+  }
+)
+
+command('Tname',
+  function()
+    vim.fn.setreg('+', vim.fn.expand('%:t'))
+  end,
+  {
+    desc = "only filename without path"
+  }
+)
 
 command('Fname',
   function()
-    vim.fn.setreg('+', vim.fn.expand('%:t'))
-  end, {})
+    vim.fn.setreg('+', vim.fn.expand('%:p'))
+  end,
+  {
+    desc = "full path filename"
+  }
+)
