6142f0a2e8dff64afa85851b0b4a8b668704406a
zengshuai <zengs1994@gmail.com>
2025-05-09 00:10:13 +0800

    update color

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -668,7 +668,7 @@ require("lazy").setup({
           ["@markup.heading.6.markdown"] = { fg = "#b7bdf8", bg = "none", bold = true, },
           ["@markup.link"] = { fg = "#8fbebb", bg = "none" },
           ["@markup.link.url"] = { fg = "#8fbcbb", bg = "none", italic = false, underline = true },
-          ["@string.special.url"] = { underdotted = true, },
+          ["@string.special.url"] = { fg = "#8fbcb9", underdotted = true, },
           ["@variable.builtin"] = { italic = false },
         }
       end,
