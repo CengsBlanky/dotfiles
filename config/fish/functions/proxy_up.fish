2cd7377cc3c5741c8e72cd54ee62360a8a7cb1c1
zengshuai <zengs1994@gmail.com>
2025-05-17 17:40:38 +0800

    update proxy_up function

--- config/fish/functions/proxy_up.fish
+++ config/fish/functions/proxy_up.fish
@@ -2,4 +2,5 @@ function proxy_up
     set -l proxy_addr http://localhost:7897
     set -gx HTTPS_PROXY $proxy_addr
     set -gx HTTP_PROXY $proxy_addr
+    set -gx NO_PROXY "localhost,127.0.0.1,10.96.0.0/12,192.168.59.0/24,192.168.49.0/24,192.168.39.0/24"
 end
