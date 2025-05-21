73f1b625c272d2e133b6c6ab7e4e789f1c13f4cf
zengshuai <zengs1994@gmail.com>
2025-05-22 00:47:52 +0800

    set kulala timeout to 3 minutes

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -576,8 +576,8 @@ require("lazy").setup({
     opts = {
       additional_curl_options = { "--insecure" },
       disable_script_print_output = true,
-      -- 1 minute timeout
-      request_timeout = 60000,
+      -- 3 minute timeout
+      request_timeout = 180000,
       urlencode = "skipencoded",
       default_env = "dev",
       ui = {
