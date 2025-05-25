d0fd2fdaf9c97685c3a16d3c29ce5f4110e75f8b
zengshuai <zengs1994@gmail.com>
2025-05-25 21:58:21 +0800

    complete java formatter: what a sick clang-format option!!!!

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -433,6 +433,7 @@ require("lazy").setup({
         kotlin = { "ktfmt" },
         xml = { "xq_format" },
         html = { "prettier" },
+        java = { "java_format" },
         javascript = { "prettier" },
         javascriptreact = { "prettier" },
         json = { "jq" },
@@ -446,6 +447,10 @@ require("lazy").setup({
         ["c_format"] = {
           command = "clang-format",
           args = "-style=file:$HOME/.config/formatter/clang-format.yaml",
+        },
+        ["java_format"] = {
+          command = "clang-format",
+          args = "-style=file:$HOME/.config/formatter/java-format.yaml --assume-filename=main.java",
         },
         ["py_format"] = {
           command = "ruff",
