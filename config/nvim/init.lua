326c03861964dfd608519430effb0a7153839d0a
zengshuai <zengs1994@gmail.com>
2025-06-06 21:39:47 +0800

    refactor

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -6,7 +6,6 @@ local map_opts = { silent = true, nowait = true }
 opt.shadafile = "NONE"
 opt.termguicolors = true
 
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
