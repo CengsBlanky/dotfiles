36147d351a1bf4a177379063c975e916447082c0
zengshuai <zengs1994@gmail.com>
2025-06-16 22:33:13 +0800

    update dbee config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -698,7 +698,7 @@ require("lazy").setup({
         },
         editor = {
           mappings = {
-            { key = "s", mode = "v", action = "run_selection" },
+            { key = "<space>r", mode = "v", action = "run_selection" },
             { key = "<space>r", mode = "n", action = "run_under_cursor" },
           },
         },
