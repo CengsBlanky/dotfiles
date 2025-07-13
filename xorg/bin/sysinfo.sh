9c81c9142542762396411aa767da0d9256b901e7
zengshuai <zengs1994@gmail.com>
2025-07-13 16:34:58 +0800

    update sysinfo script

--- xorg/bin/sysinfo.sh
+++ xorg/bin/sysinfo.sh
@@ -41,11 +41,17 @@ get_brightness() {
     echo $(brightnessctl info | sed -n '2p')
 }
 
+get_network() {
+    echo -e $(nmcli connection show --active | awk '$4 != "lo" && NR>1 {printf "%s[%s]: %s\t", $3, $4, $1}')
+}
+
+spaces="\u00A0\u00A0"
 # Output all stats line by line
-echo "  CPU temp: $(get_cpu_temp)"
-echo "󰻠  CPU Usage: $(get_cpu_usage)"
-echo "󰍛  RAM Usage: $(get_ram_info)"
-echo "󰋊  Disk Usage: $(get_disk_info)"
-echo "󱡛  Threads: $(get_thread_count)"
-echo "  $(get_sound_volumn)"
-echo "󰖨  $(get_brightness)"
+echo -e "󰻠${spaces}CPU Usage: $(get_cpu_usage)"
+echo -e "${spaces}CPU temp: $(get_cpu_temp)"
+echo -e "󰍛${spaces}RAM Usage: $(get_ram_info)"
+echo -e "󰋊${spaces}Disk Usage: $(get_disk_info)"
+echo -e "󱡛${spaces}Threads: $(get_thread_count)"
+echo -e "${spaces}$(get_sound_volumn)"
+echo -e "󰖨${spaces}$(get_brightness)"
+echo -e "${spaces}$(get_network)"
