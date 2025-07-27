30f50fbdf16c68cb7ecae3688a055742f60fe941
zengshuai <zengs1994@gmail.com>
2025-07-27 22:48:09 +0800

    update xinit config

--- xorg/bin/weather.sh
+++ xorg/bin/weather.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 LOCATION=江宁区
-while true; do
-    curl -s "http://wttr.in/$LOCATION?format=%c%t" 2>/dev/null >~/.weather
-    sleep 10m
-done
+curl -s "http://wttr.in/$LOCATION?format=%c%t" 2>/dev/null >~/.weather
