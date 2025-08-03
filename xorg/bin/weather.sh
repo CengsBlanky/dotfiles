302c005b955d34eab2870cc84805e25994fcb3e4
zengshuai <zengs1994@gmail.com>
2025-08-03 15:09:09 +0800

    update config

--- xorg/bin/weather.sh
+++ xorg/bin/weather.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 LOCATION=江宁区
-curl -x http://localhost:7890 -s "http://wttr.in/$LOCATION?format=%c%t" 2>/dev/null >~/.weather
+while true; do
+    curl -x http://localhost:7890 -s "http://wttr.in/$LOCATION?format=%c%t" 2>/dev/null >~/.weather
+    sleep 30m
+done
