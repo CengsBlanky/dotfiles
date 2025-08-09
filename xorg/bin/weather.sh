5420be36094e3c36e4a11fe1c3dc28a393273e52
zengshuai <zengs1994@gmail.com>
2025-08-09 11:29:46 +0800

    add wait

--- xorg/bin/weather.sh
+++ xorg/bin/weather.sh
@@ -2,6 +2,7 @@
 
 location=${MY_CUR_LOCATION:-"南京"}
 while true; do
+    sleep 5s
     curl -x http://localhost:7890 -s "http://wttr.in/$location?format=%c%t" 2>/dev/null >~/.weather
     sleep 30m
 done
