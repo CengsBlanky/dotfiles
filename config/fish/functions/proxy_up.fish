cf68a0c83cd72bcf8f1ffaa199040b19795754cd
zengshuai <zengs1994@gmail.com>
2025-08-16 20:15:33 +0800

    update fonts

--- config/fish/functions/proxy_up.fish
+++ config/fish/functions/proxy_up.fish
@@ -2,4 +2,6 @@ function proxy_up
     set -l proxy_addr http://127.0.0.1:7890
     set -gx HTTPS_PROXY $proxy_addr
     set -gx HTTP_PROXY $proxy_addr
+    set -gx https_proxy $proxy_addr
+    set -gx http_proxy $proxy_addr
 end
