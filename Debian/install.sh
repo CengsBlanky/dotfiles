f18baf87e012a12c2656f395fd9f330c653ec74c
zengshuai <zengs1994@gmail.com>
2025-08-16 22:14:01 +0800

    rename install file

---
+++ Debian/install.sh
@@ -0,0 +1,4 @@
+# this script install all packages managed by Debian apt manager
+# run once after Debian installed
+sudo apt install \
+    unclutter-xfixes
--- xorg/install.sh
+++
@@ -1,2 +0,0 @@
-sudo apt install \
-    unclutter-xfixes
