6c7f65369e109188ea8ae7f256050f66188b8965
zengshuai <zengs1994@gmail.com>
2025-08-16 22:06:54 +0800

    update nvim for readonly buffer

--- config/nvim/plugin/autocmd.lua
+++ config/nvim/plugin/autocmd.lua
@@ -9,8 +9,9 @@ autocmd({"FileType"}, { callback = function()
   vim.opt.formatoptions:append({'M'})
   -- set readonly map
   if not vim.bo.modifiable or vim.bo.readonly then
-    setnmap('q', '<Cmd>bd<CR>')
+    setnmap('q', '<Cmd>x<CR>')
     setnmap('u', '<C-b>')
+    setnmap('<space>', '<C-f>')
   end
 end})
 
