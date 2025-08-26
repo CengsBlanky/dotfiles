d0e55985f9a6c3bea641fbc81d403af3da5368a0
zengshuai <zengs1994@gmail.com>
2025-08-26 23:57:36 +0800

    fix script

--- xorg/bin/sysops.sh
+++ xorg/bin/sysops.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-options=" reboot
-⏻ shutdown"
+reboot=" reboot"
+shutdown="⏻ shutdown"
+options="$reboot
+$shutdown"
 chosen=$(echo -e "$options" | dmenu -i)
 case "$chosen" in
-    reboot) sudo systemctl reboot ;;
-    shutdown) sudo systemctl poweroff ;;
+    $reboot) sudo systemctl reboot ;;
+    $shutdown) sudo systemctl poweroff ;;
 esac
