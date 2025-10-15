3929bcf778e423d124487f7477ad2ec665054964
zengshuai <zengs1994@gmail.com>
2025-10-15 23:13:57 +0800

    update nvim formatter

--- config/nvim/plugin/autocmd.lua
+++ config/nvim/plugin/autocmd.lua
@@ -71,7 +71,7 @@ autocmd({"BufRead"}, {
 })
 
 autocmd({"FileType"}, {
-    pattern = { "html", "xhtml", "htmldjango", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "yaml", "sql", "json", "vim", "lua", "dart", "svelte", "http", "xml", "go" },
+    pattern = { "html", "xhtml", "htmldjango", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "yaml", "sql", "json", "jsonc", "vim", "lua", "dart", "svelte", "http", "xml", "go" },
     callback = function()
         vim.opt_local.tabstop = 2
         vim.opt_local.shiftwidth = 2
