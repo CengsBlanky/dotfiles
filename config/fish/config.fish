b71f3a81fb6e558e2a3a4592b7fe5639cbc37877
zengshuai <zengs1994@gmail.com>
2025-10-19 13:53:25 +0800

    add edit command bind for fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,7 @@
 # ===== vi mode =====
 fish_vi_key_bindings
 bind --mode insert --sets-mode default jk repaint
+bind --mode insert \ce edit_command_buffer
 set fish_cursor_default block
 set fish_cursor_insert line
 set fish_cursor_replace_one underscore
