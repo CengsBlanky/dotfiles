8845c95c9876da64fa4856d7efb35a6c515908a7
zengshuai <zengs1994@gmail.com>
2025-10-21 13:20:05 +0800

    reduce weather update time

--- xorg/bin/weather.sh
+++ xorg/bin/weather.sh
@@ -4,5 +4,5 @@ location=${MY_CUR_LOCATION:-"南京"}
 while true; do
     sleep 5s
     curl -x http://localhost:7890 -s "http://wttr.in/$location?format=%c%t" 2>/dev/null >"$XDG_RUNTIME_DIR/weather.info"
-    sleep 30m
+    sleep 20m
 done
