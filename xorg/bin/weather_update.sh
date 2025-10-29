b8c44a8a65401e5b9b165d5ca62205d29de2f1fe
zengshuai <zengs1994@gmail.com>
2025-10-30 01:32:25 +0800

    add cron script for weather

---
+++ xorg/bin/weather_update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+location=$(cat "$HOME/.local/share/location")
+if [[ -z "$location" ]]; then
+    location="南京"
+fi
+save_file="$HOME/.local/share/weather.info"
+weather=$(curl -s "http://wttr.in/$location?format=%c%t" 2>/dev/null)
+if [[ -n "$weather" ]]; then
+    echo "$location update at $(date --rfc-3339=seconds)" >$save_file
+    echo $weather >>$save_file
+fi
