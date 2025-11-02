6c8cf95ab20610f3966d9f7c21ac39afa44942a7
zengshuai <zengs1994@gmail.com>
2025-11-02 22:37:28 +0800

    update fish prompt color

--- config/fish/functions/fish_mode_prompt.fish
+++ config/fish/functions/fish_mode_prompt.fish
@@ -18,8 +18,8 @@ function fish_mode_prompt
             set_color --bold "66c0d0"
             echo -n "  "
         case default
-            set_color --bold "868e96"
-            echo -n " 󰒲 "
+            set_color --bold "b48ead"
+            echo -n "  "
         case visual
             set_color --bold "ebcb8b" 
             echo -n "  "
