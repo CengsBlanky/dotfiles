0b992e15a1b128f6a06ef583266c2a5bfcdb8cea
zengshuai <zengs1994@gmail.com>
2025-11-12 12:41:21 +0800

    update tmux config and popup script

--- tmux/script/toggle-popup.sh
+++ tmux/script/toggle-popup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-session_name="persist-popup"
+session_name='persist-popup'
+current_dir='#{pane_current_path}'
 
-tmux has-session -t "$session_name" 2>/dev/null || tmux new-session -d -s "$session_name" -c '#{pane_current_path}'
+tmux has-session -t "$session_name" 2>/dev/null || tmux new-session -d -s "$session_name" -c "$current_dir"
 
-if tmux list-sessions -F "#{session_name} #{session_attached}" | grep -w "$session_name" | grep -q '1$'; then
-    tmux detach-client
+if tmux list-sessions -F "#{session_name} #{session_attached}" | grep -q "^$session_name 1\$"; then
+    tmux detach-client -s "$session_name"
 else
-    tmux display-popup -w '80%' -h '80%' -b 'rounded' -d '#{pane_current_path}' -E "tmux attach -t $session_name -c '#{pane_current_path}'"
+    session_dir=$(tmux display-message -t $session_name -p $current_dir)
+    target_dir=$(tmux display-message -p $current_dir)
+    change_dir=""
+    if [[ "$session_dir" != "$target_dir" ]]; then
+        change_dir="tmux send-keys -t $session_name \"cd $target_dir\" C-m &&"
+    fi
+    tmux display-popup \
+        -d "$current_dir" \
+        -w '70%' -h '70%' \
+        -E "$change_dir tmux attach-session -t $session_name"
 fi
