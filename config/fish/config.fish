c0bf16ae952f331380b22b9aececa5819d3a70cc
zengshuai <zengs1994@gmail.com>
2025-11-19 15:37:57 +0800

    update path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -68,6 +68,8 @@ set -gx ADB "$ANDROID_HOME/platform-tools/adb"
 set -gx GOOGLE_CLOUD_PROJECT "initialz-250717"
 # for uv mirror
 set -gx UV_DEFAULT_INDEX "https://pypi.tuna.tsinghua.edu.cn/simple"
+# npm global bin
+fish_add_path "$HOME/.node_modules/bin"
 # nvm bin config
 fish_add_path "$HOME/.local/share/nvm/current/bin"
 # spring boot cli
