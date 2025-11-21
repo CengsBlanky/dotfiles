ea82c1fb65e3f16e50907fe7fe95eb577c5db738
zengshuai <zengs1994@gmail.com>
2025-11-21 22:28:12 +0800

    adjust lsp list

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -345,7 +345,7 @@ require("lazy").setup({
     opts= {
       automatic_enable = false,
       -- TODO kotlin_lsp too much slower
-      ensure_installed = { 'awk_ls', 'bashls', 'clangd', 'dockerls', 'lua_ls', 'marksman', 'ruff', 'basedpyright', 'djlsp', 'zls', 'gopls', 'jdtls', 'html', 'cssls', 'vtsls', 'jsonls', 'svelte', 'emmet_language_server', 'expert', 'vue_ls', },
+      ensure_installed = { 'awk_ls', 'bashls', 'clangd', 'dockerls', 'lua_ls', 'marksman', 'ruff', 'basedpyright', 'djlsp', 'zls', 'gopls', 'jdtls', 'html', 'cssls', 'vtsls', 'vue_ls', 'jsonls', 'svelte', 'emmet_language_server', 'expert', },
     },
     dependencies = {
       {
