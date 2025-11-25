d084668dd9a80fe13ecc2964d4f0412689c0d61c
zengshuai <zengs1994@gmail.com>
2025-11-25 15:29:59 +0800

    correct print error

--- config/fish/functions/showproxy.fish
+++ config/fish/functions/showproxy.fish
@@ -1,6 +1,6 @@
 function showproxy
     echo -e "HTTPS_PROXY\t$HTTPS_PROXY"
-    echo -e "http_proxy\t$HTTP_PROXY"
+    echo -e "HTTP_PROXY\t$HTTP_PROXY"
     echo -e "https_proxy\t$https_proxy"
     echo -e "http_proxy\t$http_proxy"
 end
