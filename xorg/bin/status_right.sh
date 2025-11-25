689f70d711dd693553912ceaabcda1721c4e6a29
zengshuai <zengs1994@gmail.com>
2025-11-26 00:08:56 +0800

    perfect sys status bar

--- xorg/bin/status_right.sh
+++ xorg/bin/status_right.sh
@@ -2,7 +2,7 @@
 
 volume() {
     vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
-    if grep -q '[MUTED]' <<< "$vol"; then
+    if grep -q '[MUTED]' <<<"$vol"; then
         printf "  "
     else
         printf "  %.0f%%" $(echo "$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -oE '[0-9]+\.[0-9]+') * 100" | bc)
@@ -17,15 +17,27 @@ battery() {
     batlink=$(find /sys/class/power_supply/ -name "BAT[0-9]" | tail -1)
     bat_status=$(cat $batlink/status)
     percent=$(cat $batlink/capacity)
-    bat=""
-    bat="🔋$percent%"
-    return $bat
+    if [[ "Not charging" == "$bat_status" && $percent -lt 80 ]]; then
+        printf " 🔋 %s%%" "$percent"
+    fi
 }
 
+localtime() {
+    printf "🗓 $(date '+%Y-%m-%d %H:%M %a')"
+}
+
+weather() {
+    info=$(sed -n '2p' "$HOME/.local/share/weather.info" 2>/dev/null)
+    printf "$info"
+}
+
+status() {
+    xsetroot -name "$(volume)$(brightness)  $(weather) $(localtime)$(battery)"
+}
+
+# wait for preparation
+sleep 1s
 while true; do
-    sleep 2s
-    LOCALTIME=$(date '+%Y-%m-%d %H:%M %a')
-    WEATHER=$(sed -n '2p' "$HOME/.local/share/weather.info" 2>/dev/null)
-    xsetroot -name "$(volume)$(brightness)  $WEATHER $(battery) 🗓 $LOCALTIME"
+    $(status)
     sleep 25s
 done
