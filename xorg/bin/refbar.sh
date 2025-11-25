4d24cac4794178cdbdbe468c338e40ae61d33e64
zengshuai <zengs1994@gmail.com>
2025-11-25 23:37:09 +0800

    check before kill

--- xorg/bin/refbar.sh
+++ xorg/bin/refbar.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 barpid=$(pstree -lp | grep status_right | sed "s/.*sleep(\([0-9]\+\)).*/\1/")
-kill "$barpid"
+# kill if sleeping
+if [[ -n "$barpid" && "$barpid" =~ ^[0-9]+$ ]]; then
+    kill "$barpid"
+fi
