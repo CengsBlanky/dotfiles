c778ed28ef8de16967fa8a3ba59f903192f87ae5
zengshuai <zengs1994@gmail.com>
2025-12-04 11:48:56 +0800

    update lua_ls config

--- config/nvim/lsp/lua_ls.lua
+++ config/nvim/lsp/lua_ls.lua
@@ -2,10 +2,10 @@ return {
   settings = {
     Lua = {
       workspace = {
-        library = vim.tbl_filter(
-          function (d) return not d:match(vim.fn.stdpath("config") .. "/?a?f?t?e?r?") end,
-          vim.api.nvim_get_runtime_file("", true)
-        ),
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+        },
       },
     },
   },
