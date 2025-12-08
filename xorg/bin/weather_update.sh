15aefb377f6f4c9c3fcf6c877dff478f0b5255ae
zengshuai <zengs1994@gmail.com>
2025-12-08 12:28:15 +0800

    update weather location script

--- xorg/bin/weather_update.sh
+++ xorg/bin/weather_update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 location=$(cat "$HOME/.local/share/location")
-if [[ -z "$location" ]]; then
-    location=$(curl --noproxy '*' -X 'GET' -s 'http://ip-api.com/json/?lang=zh-CN' | jq -r '.city')
-fi
+baidu_location=$(curl --noproxy -X 'GET' -s --insecure 'https://api.map.baidu.com/location/ip?coor=bd09ll&ak=nGMOlIE09UWDVhflZaN0Vz5hqIEl0bjj')
+point_info=$(echo $baidu_location | jq -r '.content.point.y + "," + .content.point.x')
+address_info=$(echo $baidu_location | jq -r '.address')
 save_file="$HOME/.local/share/weather.info"
-weather=$(curl -s "http://wttr.in/$location?format=%c%t" 2>/dev/null)
+weather=$(curl -s "http://wttr.in/$point_info?format=%c%t" 2>/dev/null)
 if [[ -n "$weather" ]]; then
-    echo "$location update at $(date --rfc-3339=seconds)" >$save_file
+    echo "$address_info update at $(date --rfc-3339=seconds)" >$save_file
     echo $weather >>$save_file
 fi
