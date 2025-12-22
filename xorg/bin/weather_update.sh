5e97174fdf78eefcd6fbbb62b6e35bb1d9c4a55c
zengshuai <zengs1994@gmail.com>
2025-12-22 21:06:00 +0800

    update weather update script

--- xorg/bin/weather_update.sh
+++ xorg/bin/weather_update.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 baidu_location=$(curl --noproxy -X 'GET' -s --insecure 'https://api.map.baidu.com/location/ip?coor=bd09ll&ak=nGMOlIE09UWDVhflZaN0Vz5hqIEl0bjj')
-point_info=$(echo $baidu_location | jq -r '.content.point.y + "," + .content.point.x')
+coordinate=$(echo $baidu_location | jq -r '.content.point.y + "," + .content.point.x')
 address_info=$(echo $baidu_location | jq -r '.address')
 save_file="$HOME/.local/share/weather.info"
-weather=$(curl -s "http://wttr.in/$point_info?format=%c%t" 2>/dev/null)
+weather=$(curl -s "http://wttr.in/$coordinate?format=%c%t" 2>/dev/null)
 if [[ -n "$weather" ]]; then
     echo "$address_info update at $(date --rfc-3339=seconds)" >$save_file
     echo $weather >>$save_file
+else
+    # can not get weather info mostly because of internet connection error
+    echo "" >$save_file
 fi
