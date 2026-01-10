7afef161a5a21d96d201cb14c0f0cc4c69510678
zengshuai <zengs1994@gmail.com>
2026-01-10 18:17:27 +0800

    update proxy config

--- config/fish/functions/onproxy.fish
+++ config/fish/functions/onproxy.fish
@@ -1,5 +1,5 @@
 function onproxy
-    set -l proxy_addr http://127.0.0.1:7890
+    set -l proxy_addr http://127.0.0.1:7897
     set -gx HTTPS_PROXY $proxy_addr
     set -gx HTTP_PROXY $proxy_addr
     set -gx https_proxy $proxy_addr
