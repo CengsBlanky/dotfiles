499a951dbf7c60d74620256c0abdf744c00c6432
zengshuai <zengs1994@gmail.com>
2026-01-14 21:59:32 +0800

    fix fish sysupdate script

--- config/fish/functions/sysupdate.fish
+++ config/fish/functions/sysupdate.fish
@@ -1,9 +1,9 @@
 function sysupdate
-    npm -g update
-    pipx upgrade-all
     sudo pacman -Syu && sudo pacman -Sc --noconfirm
     onproxy
     yay -Sua && yay -Sc
     paru -Sua
     offproxy
+    npm -g update
+    pipx upgrade-all; or pipx reinstall-all
 end
