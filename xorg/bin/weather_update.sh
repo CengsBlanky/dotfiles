29a2754678ff04c42e77e3833f9f4e91a2a37a2b
zengshuai <zengs1994@gmail.com>
2026-01-17 20:04:38 +0800

    update weather update for error log

--- xorg/bin/weather_update.sh
+++ xorg/bin/weather_update.sh
@@ -5,7 +5,8 @@ baidu_location=$(curl --noproxy -X 'GET' -s --insecure "$baidu_loc_req")
 coordinate=$(echo $baidu_location | jq -r '.content.point.y + "," + .content.point.x')
 address_info=$(echo $baidu_location | jq -r '.address')
 save_file="$HOME/.local/share/weather.info"
-weather=$(curl -s "http://wttr.in/$coordinate?format=%c%t" 2>/dev/null)
+tmp_err_file=$(mktemp)
+weather=$(curl -s "http://wttr.in/$coordinate?format=%c%t" 2>$tmp_err_file)
 echo "$address_info update at $(date --rfc-3339=seconds)" >$save_file
 if [[ -n "$weather" ]]; then
     echo $weather >>$save_file
@@ -14,5 +15,18 @@ else
     baidu_ak=$(cat "$HOME/.local/share/baidu_ak" | tr -d '\n\t')
     req_location=$(echo $baidu_location | jq -r '.content.point.x + "," + .content.point.y')
     baidu_weather_req="https://api.map.baidu.com/weather/v1/?location=$req_location&data_type=now&ak=$baidu_ak"
-    curl --noproxy -X 'GET' -s --insecure "$baidu_weather_req" | jq -r '.result.now.text + " " + (.result.now.temp | tostring) + "℃"' >>$save_file
+    weather_rsp=$(curl --noproxy -X 'GET' -s --insecure "$baidu_weather_req" 2>&1)
+    rsp_status=$(echo "$weather_rsp" | jq -r '.status')
+    if [[ "0" -eq "$rsp_status" ]]; then
+        echo "$weather_rsp" | jq -r '.result.now.text + " " + (.result.now.temp | tostring) + "℃"' >>$save_file
+    else
+        echo "baidu api failed response: $weather_rsp" >$tmp_err_file
+    fi
+fi
+
+if [[ -f "$tmp_err_file" ]]; then
+    error_log="$HOME/.local/share/weatherUpdateErr.log"
+    echo "$(date --rfc-3339=seconds) [ERROR]:" >$error_log
+    cat "$tmp_err_file" >>$error_log
+    rm $tmp_err_file
 fi
