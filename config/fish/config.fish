f2f672df832ec25f237adb17308f419c2dbcd92b
zengshuai <zengs1994@gmail.com>
2026-02-09 17:58:43 +0800

    add abbr to force delete

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -158,6 +158,8 @@ abbr --add clock "tty-clock -c -C 4 -S"
 abbr --add qianwen --set-cursor "qwen '%' | glow -"
 # play music
 abbr --add music "ncmpcpp"
+# force delete
+abbr --add frm "sudo rm -rf"
 
 # source asdf version manager
 set -l asdf_conf "$HOME/.asdf/asdf.fish"
