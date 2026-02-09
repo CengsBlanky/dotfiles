42fd104dd1bd639b68e632ccaadcc8cb91990502
zengshuai <zengs1994@gmail.com>
2026-02-09 19:56:38 +0800

    update fish function about transmission

--- config/fish/functions/stoptrans.fish
+++ config/fish/functions/stoptrans.fish
@@ -1,8 +1,8 @@
 function stoptrans
     set -l SERVICE_NAME "transmission-daemon.service"
     if systemctl is-active --quiet $SERVICE_NAME
-        echo "stopping $SERVICE_NAME..."
         sudo systemctl stop $SERVICE_NAME
+        echo "$SERVICE_NAME stopped."
     else
         echo "$SERVICE_NAME is not running."
     end
--- config/fish/functions/transtorrent.fish
+++ config/fish/functions/transtorrent.fish
@@ -1,11 +1,10 @@
 function transtorrent
     set -l SERVICE_NAME "transmission-daemon.service"
     echo "start $SERVICE_NAME..."
-    sleep 0.5s
-    if systemctl is-active --quiet $SERVICE_NAME
-        open "http://localhost:9091" &
-    else
+    if not systemctl is-active --quiet $SERVICE_NAME
         sudo systemctl start $SERVICE_NAME
+        sleep 0.5s
     end
+    open "http://localhost:9091"
 end
 
