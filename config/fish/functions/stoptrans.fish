f1e84f4728a4279b4e47f0fb3c914f3bea0960a4
zengshuai <zengs1994@gmail.com>
2026-03-12 22:44:04 +0800

    update transmission-cli function

--- config/fish/functions/stoptrans.fish
+++
@@ -1,9 +0,0 @@
-function stoptrans
-    set -l SERVICE_NAME "transmission-daemon.service"
-    if systemctl is-active --quiet $SERVICE_NAME
-        sudo systemctl stop $SERVICE_NAME
-        echo "$SERVICE_NAME stopped."
-    else
-        echo "$SERVICE_NAME is not running."
-    end
-end
