5757020b909e5f79721307ef9cf499d94cca4712
zengshuai <zengs1994@gmail.com>
2026-04-09 17:28:58 +0800

    add jumpwindow script to use dmenu to jump to selected one

---
+++ xorg/bin/jumpwindow.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+windows=$(wmctrl -l | grep -v '^$')
+declare -A win_map
+menu_items=()
+index=1
+
+while IFS=' ' read -r win_id desktop host window_name; do
+    win_map["$index"]="$win_id"
+    menu_items+=("$index. $window_name")
+    ((index++))
+done <<<"$windows"
+
+selected=$(printf "%s\n" "${menu_items[@]}" | dmenu -i)
+
+[ -z "$selected" ] && exit 0
+
+sel_index=$(echo "$selected" | cut -d'.' -f1)
+target_id=${win_map[$sel_index]}
+wmctrl -i -a "$target_id"
