7ea029daee065a0e9072ef057314847d4d1ef4fe
zengshuai <zengs1994@gmail.com>
2026-04-09 15:56:57 +0800

    dont show proxy address

--- config/fish/functions/onproxy.fish
+++ config/fish/functions/onproxy.fish
@@ -1,6 +1,5 @@
 function onproxy
     read -l proxy_addr < $HOME/.local/share/proxy.info
-    echo "set proxy address to: $proxy_addr"
     set -gx HTTPS_PROXY $proxy_addr
     set -gx HTTP_PROXY $proxy_addr
     set -gx https_proxy $proxy_addr
