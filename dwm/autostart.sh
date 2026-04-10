b2a5cc6063f55a971c1be97873082ce770be288f
zengshuai <zengs1994@gmail.com>
2026-04-10 19:45:00 +0800

    update alacritty config

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -3,5 +3,5 @@
 /usr/bin/keepassxc --keyfile ~/.local/share/secret/system.key ~/.local/share/keyrings/system.kdbx &
 $HOME/.local/bin/eudic.AppImage &
 /usr/bin/firefox &
-/usr/bin/alacritty &
+alacritty &
 # /usr/bin/clash-verge &
