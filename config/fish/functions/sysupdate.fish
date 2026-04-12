bc50a462302a021a1fc901e1d881c4e9c3582b0b
zengshuai <zengs1994@gmail.com>
2026-04-12 19:24:05 +0800

    update completion after upgrade

--- config/fish/functions/sysupdate.fish
+++ config/fish/functions/sysupdate.fish
@@ -4,12 +4,16 @@ function sysupdate
     echo " npm update:"
     npm update -g
     echo " bun update:"
-    bun upgrade && bun update --global --latest
+    bun upgrade && bun completions && bun update --global --latest
     echo " pnpm update:"
     pnpm self-update && pnpm update --global
+    pnpm completion fish >~/.config/fish/completions/pnpm.fish
     echo " pipx update:"
     pipx upgrade-all; or pipx reinstall-all
+    register-python-argcomplete --shell fish pipx >~/.config/fish/completions/pipx.fish
     echo " uv update:"
     uv self update && uv tool upgrade --all
+    uv generate-shell-completion fish >~/.config/fish/completions/uv.fish
+    uvx --generate-shell-completion fish >~/.config/fish/completions/uvx.fish
     offproxy
 end
