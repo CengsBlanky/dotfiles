3d2ebfab953e7eaab4754b709d34aaa35d4850ed
zengshuai <zengs1994@gmail.com>
2026-04-20 01:18:19 +0800

    add fish script to watch video streams

---
+++ config/fish/functions/kan.fish
@@ -0,0 +1,21 @@
+function kan
+    # watch online stream
+    read -l proxy_addr < $HOME/.local/share/proxy.info
+    set -l metajson (yt-dlp --proxy="$proxy_addr" -j "$argv" 2>/dev/null)
+    if test -z "$metajson"
+        echo "Failed to fetch metadata"
+        return 1
+    end
+
+    set -l title (echo "$metajson" | jq -r '"\(.title) - \(.uploader)"')
+
+    yt-dlp -q --no-warnings \
+    --proxy "$proxy_addr" \
+    --format "bestvideo[height<=720]+bestaudio/best[height<=720]" \
+    -o - "$argv" |
+    mpv \
+    --script=/etc/mpv/scripts/sponsorblock_minimal.lua \
+    --force-media-title="$title" \
+    --force-seekable=yes \
+    -
+end
