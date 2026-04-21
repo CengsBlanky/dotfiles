fd70b9d6c197e1053c9f3d84ec9664c027e0b161
zengshuai <zengs1994@gmail.com>
2026-04-21 16:51:59 +0800

    update yt-dlp download option

--- config/fish/functions/yt-audio.fish
+++ config/fish/functions/yt-audio.fish
@@ -1,3 +1,4 @@
 function yt-audio
-    yt-dlp --config-location ~/.config/yt-dlp/audio_config "$argv"
+    set -l proxy_addr (grep -m 1 -e "^socks" $HOME/.local/share/proxy.info | tr -d "\n")
+    yt-dlp --config-location ~/.config/yt-dlp/audio_config --proxy $proxy_addr "$argv"
 end
--- config/fish/functions/yt-subtitle.fish
+++ config/fish/functions/yt-subtitle.fish
@@ -1,3 +1,4 @@
 function yt-subtitle
-    yt-dlp --config-location ~/.config/yt-dlp/subtitle_config "$argv"
+    set -l proxy_addr (grep -m 1 -e "^socks" $HOME/.local/share/proxy.info | tr -d "\n")
+    yt-dlp --config-location ~/.config/yt-dlp/subtitle_config --proxy $proxy_addr "$argv"
 end
