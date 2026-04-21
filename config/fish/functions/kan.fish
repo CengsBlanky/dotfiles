ce6e77b10af4d9bf43fefece35267b1554882726
zengshuai <zengs1994@gmail.com>
2026-04-21 22:04:48 +0800

    do not separate audio with video

--- config/fish/functions/kan.fish
+++ config/fish/functions/kan.fish
@@ -1,6 +1,7 @@
 function kan
     # watch online stream
     set -l proxy_addr (grep -m 1 -e "^socks" $HOME/.local/share/proxy.info | tr -d "\n")
+    echo "using proxy: $proxy_addr"
     set -l metajson (yt-dlp --proxy="$proxy_addr" -j "$argv" 2>/dev/null)
     if test -z "$metajson"
         echo "Failed to fetch metadata"
@@ -11,7 +12,7 @@ function kan
 
     yt-dlp -q --no-warnings \
     --proxy "$proxy_addr" \
-    --format "bestvideo[height<=720]+bestaudio/best[height<=720]" \
+    --format "best[height<=720]/best[height<=1080]/best" \
     --remote-components ejs:npm \
     --embed-subs \
     -o - "$argv" |
