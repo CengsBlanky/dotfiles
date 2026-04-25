77f171f40cb84d33bcf0943abcf27af52cc5c70a
zengshuai <zengs1994@gmail.com>
2026-04-25 16:04:38 +0800

    adjust yt-dlp download param

--- config/fish/functions/fish_mode_prompt.fish
+++ config/fish/functions/fish_mode_prompt.fish
@@ -3,11 +3,19 @@ function fish_mode_prompt
     set -l last_status $status
     set -l pwd_color_bare 88c0d0
     set -l err_color c92a2a
-    set -l prompt_icon_color a3be8c
 
     set -l insert_color 66c0d0
+    set -l default_color 5e81ac
+    set -l visual_color ebcb8b
+    set -l replace_color brmagenta
+    set -l fallback_color red
+    set -l proxy_color 8686bf
     if test -n "$HTTPS_PROXY"
-        set insert_color 8686bf
+        set insert_color $proxy_color
+        set default_color $proxy_color
+        set visual_color $proxy_color
+        set replace_color $proxy_color
+        set fallback_color $proxy_color
     end
 
     set_color -o $pwd_color_bare
@@ -23,16 +31,16 @@ function fish_mode_prompt
             set_color --bold $insert_color
             echo -n "  "
         case default
-            set_color --bold 5e81ac
+            set_color --bold $default_color
             echo -n "  "
         case visual
-            set_color --bold ebcb8b
+            set_color --bold $visual_color
             echo -n "  "
         case replace_one
-            set_color --bold brmagenta
+            set_color --bold $replace_color
             echo -n '  '
         case '*'
-            set_color --bold red
+            set_color --bold $fallback_color
             echo -n '  '
     end
     set_color -b normal
