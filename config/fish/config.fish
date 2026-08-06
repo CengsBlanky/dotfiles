284e75a3f1c3b68900393fae0d7e9aec90e77177
zengshuai <zengs1994@gmail.com>
2026-08-06 18:04:31 +0800

    update mihomo cofig

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -183,4 +183,6 @@ fish_add_path "$HOME/.config/emacs/bin"
 # exa config
 set -gx EXA_COLORS "*.mp3=38;2;227;138;174:*.webm=38;2;129;161;193:*.mp3=38;2;129;161;193:*.flac=38;2;143;188;187"
 
-$HOME/.local/bin/mise activate fish | source # added by https://mise.run/fish
+if [ -f "$HOME/.local/bin/mise" ]
+    $HOME/.local/bin/mise activate fish | source # added by https://mise.run/fish
+end
