316a81a8356fe9809da71d1c328512ff51064826
Shawn <52321832+z3sh3@users.noreply.github.com>
2026-08-10 16:42:38 +0800

    update windows nvim plugin config

--- Windows/nvim/lua/plugins.lua
+++ Windows/nvim/lua/plugins.lua
@@ -5,20 +5,31 @@ vim.pack.add({
   {
     src = "https://github.com/numToStr/Comment.nvim",
   },
+  {
+    src = "https://github.com/preservim/nerdtree",
+  },
 },
 {
   load = true,
 })
 
-local configs = {
-    "nvim-surround",
-    "Comment.nvim",
+require("nvim-surround").setup({})
+-- nerdtree config
+vim.g.NERDTreeStatusline='  '
+vim.g.NERDTreeWinSize = 40
+vim.g.NERDTreeQuitOnOpen = 3
+vim.g.NERDTreeMinimalUI = 1
+vim.g.NERDTreeHighlightCursorline = 1
+vim.g.NERDTreeMinimalMenu = 1
+vim.g.NERDTreeAutoDeleteBuffer = 1
+vim.g.NERDTreeCaseSensitiveFS = 1
+vim.g.NERDTreeHighlightCursorline = 1
+vim.g.NERDTreeShowLineNumbers = 1
+vim.g.NERDTreeIgnore = {
+  '\\.o$[[file]]', '\\.out$[[file]]', '\\.class$[[file]]', '\\.exe$[[file]]', '\\.jar$[[file]]', '\\.tar$[[file]]', '\\.gz$[[file]]', '\\.7z$[[file]]',
+  '^node_modules$[[dir]]', '^dist$[[dir]]', '^packages$[[dir]]', '^target$[[dir]]', '^__pycache__$[[dir]]'
 }
-
-for _, plugin in ipairs(configs) do
-    local ok, module = pcall(require, plugin)
-
-    if ok and module.setup then
-        module.setup({})
-    end
-end
+local map_opts = { noremap = true, silent = true, }
+vim.keymap.set("n", "<Tab>",  "<cmd>NERDTreeToggle<CR>", map_opts)
+vim.keymap.set("n", "<C-s>",  "<cmd>NERDTreeFind<CR>",  map_opts)
+-- nerdtree config end
