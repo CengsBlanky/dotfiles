f9f8aaabf1f7bff723958a75f17ed1648af1c2b7
zengshuai <zengs1994@gmail.com>
2026-08-30 21:26:25 +0800

    update config

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 /usr/bin/keepassxc --keyfile ~/.local/share/secret/system.key ~/.local/share/keyrings/system.kdbx &
-$HOME/.local/bin/eudic.AppImage &
 /usr/bin/firefox &
 alacritty &
